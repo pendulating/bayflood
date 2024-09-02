669b65ff3207f0adc6e3856d22ea4c42812a68c2
epierson9 <2plus2make5@gmail.com>
2024-09-02 17:09:42 -0400

    location specific model might be working...it recovers the true params on synthetic data, but needs review and also I think the way you're incrementing target is a bit sketchy, or at least inconsistent, with how you're incrementing the other thing

--- emma_util.py
+++ emma_util.py
@@ -43,7 +43,8 @@ def read_real_data(single_compartment_for_debugging=False):
 def generate_simulated_data(N, images_per_location, 
                             total_annotated_classified_negative, 
                             total_annotated_classified_positive, 
-                            icar_prior_setting):
+                            icar_prior_setting, 
+                            annotations_have_locations):
     """
     Generate simulated data for the model.
     """    
@@ -77,14 +78,22 @@ def generate_simulated_data(N, images_per_location,
     p_y_hat_1_given_y_1 = random.random() * 0.5 + 0.2
     p_y_hat_1_given_y_0 = random.random() * 0.01 + 0.01
 
+    n_true_positive_by_area = []
+    n_true_positive_classified_positive_by_area = []
+    n_true_positive_classified_negative_by_area = []
+    n_true_negative_classified_positive_by_area = []
+    n_true_negative_classified_negative_by_area = []
     n_classified_positive_by_area = []
-    n_true_positive = []
+    
     for i in range(N):
-        n_true_positive.append(np.random.binomial(n_images_by_area[i], p_Y[i]))
-        n_classified_positive_by_area.append(np.random.binomial(n_true_positive[-1], p_y_hat_1_given_y_1) + 
-                                    np.random.binomial(n_images_by_area[i] - n_true_positive[-1], p_y_hat_1_given_y_0))
+        n_true_positive_by_area.append(np.random.binomial(n_images_by_area[i], p_Y[i]))
+        n_true_positive_classified_positive_by_area.append(np.random.binomial(n_true_positive_by_area[i], p_y_hat_1_given_y_1))
+        n_true_positive_classified_negative_by_area.append(n_true_positive_by_area[i] - n_true_positive_classified_positive_by_area[i])
+        n_true_negative_classified_positive_by_area.append(np.random.binomial(n_images_by_area[i] - n_true_positive_by_area[i], p_y_hat_1_given_y_0))
+        n_true_negative_classified_negative_by_area.append(n_images_by_area[i] - n_true_positive_by_area[i] - n_true_negative_classified_positive_by_area[i])
+        n_classified_positive_by_area.append(n_true_positive_classified_positive_by_area[i] + n_true_negative_classified_positive_by_area[i])
     empirical_p_yhat = sum(n_classified_positive_by_area) * 1.0 / sum(n_images_by_area)
-    empirical_p_y = sum(n_true_positive) * 1.0 / sum(n_images_by_area)
+    empirical_p_y = sum(n_true_positive_by_area) * 1.0 / sum(n_images_by_area)
     p_y_1_given_y_hat_1 = p_y_hat_1_given_y_1 * empirical_p_y / empirical_p_yhat
     p_y_1_given_y_hat_0 = (1 - p_y_hat_1_given_y_1) * empirical_p_y / (1 - empirical_p_yhat)
     print("empirical_p_y", empirical_p_y)
@@ -93,20 +102,63 @@ def generate_simulated_data(N, images_per_location,
     print("p_y_hat_1_given_y_0", p_y_hat_1_given_y_0)
     print("p_y_1_given_y_hat_1", p_y_1_given_y_hat_1)
     print("p_y_1_given_y_hat_0", p_y_1_given_y_hat_0)
-                     
-    total_annotated_classified_negative_true_positive = np.random.binomial(total_annotated_classified_negative, p_y_1_given_y_hat_0)
-    total_annotated_classified_positive_true_positive = np.random.binomial(total_annotated_classified_positive, p_y_1_given_y_hat_1)
-    print("number of annotated classified negative which were positive: %i/%i" % (total_annotated_classified_negative_true_positive, total_annotated_classified_negative))
-    print("number of annotated classified positive which were positive: %i/%i" % (total_annotated_classified_positive_true_positive, total_annotated_classified_positive))
-    
 
-    return {'observed_data':{'N':N, 'N_edges':len(node1), 'node1':node1, 'node2':node2, 
+    if not annotations_have_locations:
+        total_annotated_classified_negative_true_positive = np.random.binomial(total_annotated_classified_negative, p_y_1_given_y_hat_0)
+        total_annotated_classified_positive_true_positive = np.random.binomial(total_annotated_classified_positive, p_y_1_given_y_hat_1)
+        print("number of annotated classified negative which were positive: %i/%i" % (total_annotated_classified_negative_true_positive, total_annotated_classified_negative))
+        print("number of annotated classified positive which were positive: %i/%i" % (total_annotated_classified_positive_true_positive, total_annotated_classified_positive))
+        observed_data = {'N':N, 'N_edges':len(node1), 'node1':node1, 'node2':node2, 
                              'n_images_by_area':n_images_by_area, 'n_classified_positive_by_area':n_classified_positive_by_area, 
                              'total_annotated_classified_negative':total_annotated_classified_negative,
                                 'total_annotated_classified_positive':total_annotated_classified_positive,
                                 'total_annotated_classified_negative_true_positive':total_annotated_classified_negative_true_positive,
-                                'total_annotated_classified_positive_true_positive':total_annotated_classified_positive_true_positive},
+                                'total_annotated_classified_positive_true_positive':total_annotated_classified_positive_true_positive}
+    else:
+        n_classified_positive_annotated_by_area = np.random.multinomial(total_annotated_classified_positive, np.array(n_classified_positive_by_area)/sum(n_classified_positive_by_area))
+        assert n_classified_positive_annotated_by_area.sum() == total_annotated_classified_positive
+        ps = np.array(n_images_by_area) - np.array(n_classified_positive_by_area)
+        ps = ps / sum(ps)
+        n_classified_negative_annotated_by_area = np.random.multinomial(total_annotated_classified_negative, ps)
+        assert n_classified_negative_annotated_by_area.sum() == total_annotated_classified_negative
+        n_classified_positive_annotated_positive_by_area = []
+        n_classified_positive_annotated_negative_by_area = []
+        n_classified_negative_annotated_positive_by_area = []
+        n_classified_negative_annotated_negative_by_area = []
+        n_non_annotated_by_area = []
+        n_non_annotated_by_area_classified_positive = []
+        for i in range(N):
+            if n_classified_positive_annotated_by_area[i] > 0:
+                vector_to_sample = [1] * n_true_positive_classified_positive_by_area[i] + [0] * n_true_negative_classified_positive_by_area[i]
+                samples = np.random.choice(vector_to_sample, n_classified_positive_annotated_by_area[i], replace=False)
+                assert len(samples) == n_classified_positive_annotated_by_area[i]
+                n_classified_positive_annotated_positive_by_area.append(sum(samples))
+                n_classified_positive_annotated_negative_by_area.append(n_classified_positive_annotated_by_area[i] - sum(samples))
+            else:
+                n_classified_positive_annotated_positive_by_area.append(0)
+                n_classified_positive_annotated_negative_by_area.append(0)
+            if n_classified_negative_annotated_by_area[i] > 0:
+                vector_to_sample = [1] * n_true_positive_classified_negative_by_area[i] + [0] * n_true_negative_classified_negative_by_area[i]
+                samples = np.random.choice(vector_to_sample, n_classified_negative_annotated_by_area[i], replace=False)
+                assert len(samples) == n_classified_negative_annotated_by_area[i]
+                n_classified_negative_annotated_positive_by_area.append(sum(samples))
+                n_classified_negative_annotated_negative_by_area.append(n_classified_negative_annotated_by_area[i] - sum(samples))
+            else:
+                n_classified_negative_annotated_positive_by_area.append(0)
+                n_classified_negative_annotated_negative_by_area.append(0)
+            n_non_annotated_by_area.append(n_images_by_area[i] - n_classified_negative_annotated_by_area[i] - n_classified_positive_annotated_by_area[i])
+            n_non_annotated_by_area_classified_positive.append(n_classified_positive_by_area[i] - n_classified_positive_annotated_by_area[i])
 
+        observed_data = {'N':N, 'N_edges':len(node1), 'node1':node1, 'node2':node2, 
+                             'n_images_by_area':n_images_by_area, 'n_classified_positive_by_area':n_classified_positive_by_area, 
+                                'n_classified_positive_annotated_positive_by_area':n_classified_positive_annotated_positive_by_area,
+                                'n_classified_positive_annotated_negative_by_area':n_classified_positive_annotated_negative_by_area,
+                                'n_classified_negative_annotated_positive_by_area':n_classified_negative_annotated_positive_by_area,
+                                'n_classified_negative_annotated_negative_by_area':n_classified_negative_annotated_negative_by_area, 
+                                'n_non_annotated_by_area':n_non_annotated_by_area,
+                                'n_non_annotated_by_area_classified_positive':n_non_annotated_by_area_classified_positive}
+
+    return {'observed_data':observed_data,
             'parameters':{'phi':phi, 'phi_offset':phi_offset, 
                           'p_y_1_given_y_hat_1':p_y_1_given_y_hat_1,
                             'p_y_1_given_y_hat_0':p_y_1_given_y_hat_0, 
---
+++ stan_models/weighted_ICAR_prior_annotations_have_locations.stan
@@ -0,0 +1,58 @@
+
+// Stan code below works and has been reviewed but does not implement a proper ICAR prior. Set variable use_ICAR_prior to 0 if you don't want to use one at all. 
+// I think it might also be principled to set the ICAR prior weight to 0.5, https://mc-stan.org/users/documentation/case-studies/icar_stan.html. 
+// input data:
+// ['N', 'N_edges', 'node1', 'node2', 'n_images_by_area', 
+// 'n_classified_positive_by_area', 'n_classified_positive_annotated_positive_by_area', 
+// 'n_classified_positive_annotated_negative_by_area', 'n_classified_negative_annotated_positive_by_area', 
+// 'n_classified_negative_annotated_negative_by_area', 'use_ICAR_prior', 'ICAR_prior_weight'])
+
+data {
+  int<lower=0> N; // number of Census tracts. 
+  int<lower=0> N_edges; // number of edges in the graph (i.e. number of pairs of adjacent Census tracts). 
+  array[N_edges] int<lower=1, upper=N> node1; // node1[i] adjacent to node2[i]
+  array[N_edges] int<lower=1, upper=N> node2; // and node1[i] < node2[i]
+  array[N] int<lower=0> n_images_by_area; // vector with one entry per Census tract of the number of images in that tract. 
+  array[N] int<lower=0> n_classified_positive_by_area; // vector with one entry per Census tract of number of images classified positive. 
+  vector[N] n_classified_positive_annotated_positive_by_area;
+  vector[N] n_classified_positive_annotated_negative_by_area;
+  vector[N] n_classified_negative_annotated_positive_by_area;
+  vector[N] n_classified_negative_annotated_negative_by_area;
+  array[N] int<lower=0> n_non_annotated_by_area;
+  array[N] int<lower=0> n_non_annotated_by_area_classified_positive;
+
+  int<lower=0,upper=1> use_ICAR_prior; // 1 if you want to use ICAR prior, 0 if you don't. ICAR prior basically smooths the data. 
+  real <lower=0> ICAR_prior_weight; // weight of ICAR prior.
+}
+parameters {
+  vector[N] phi;
+  real<upper=0> phi_offset; // this is the mean from which phis are drawn. Upper bound at 0 to rule out bad modes and set prior that true positives are rare. 
+  ordered[2] logit_p_y_1_given_y_hat; // ordered to impose the constraint that p_y_1_given_y_hat_0 < p_y_1_given_y_hat_1.
+}
+transformed parameters {
+    real p_y_1_given_y_hat_0 = inv_logit(logit_p_y_1_given_y_hat[1]);
+    real p_y_1_given_y_hat_1 = inv_logit(logit_p_y_1_given_y_hat[2]);
+    vector[N] p_y = inv_logit(phi);
+    real empirical_p_yhat = sum(n_classified_positive_by_area) * 1.0 / sum(n_images_by_area);
+    real p_y_hat_1_given_y_1 = empirical_p_yhat * p_y_1_given_y_hat_1 / (empirical_p_yhat * p_y_1_given_y_hat_1 + (1 - empirical_p_yhat) * p_y_1_given_y_hat_0);
+    real p_y_hat_1_given_y_0 = empirical_p_yhat * (1 - p_y_1_given_y_hat_1) / (empirical_p_yhat * (1 - p_y_1_given_y_hat_1) + (1 - empirical_p_yhat) * (1 - p_y_1_given_y_hat_0));
+}
+model {
+  // You can't just scale ICAR priors by random numbers; the only principled value for ICAR_prior_weight is 0.5. 
+  // https://stats.stackexchange.com/questions/333258/strength-parameter-in-icar-spatial-model
+  // still, there's no computational reason you can't use another value. 
+  if (use_ICAR_prior == 1) {
+    target += -ICAR_prior_weight * dot_self(phi[node1] - phi[node2]);
+  }
+  
+  // model the classification results by Census tract for the UNANNOTATED images. 
+  phi_offset ~ normal(0, 2);
+  phi ~ normal(phi_offset, 1); 
+  n_non_annotated_by_area_classified_positive ~ binomial(n_non_annotated_by_area, p_y .* p_y_hat_1_given_y_1 + (1 - p_y) .* p_y_hat_1_given_y_0);
+
+  // model the annotation results by Census tract. 
+  target += (sum(n_classified_positive_annotated_positive_by_area .* log(p_y * p_y_hat_1_given_y_1))
+    + sum(n_classified_negative_annotated_positive_by_area .* log(p_y * (1 - p_y_hat_1_given_y_1)))
+    + sum(n_classified_positive_annotated_negative_by_area .* log((1 - p_y) * p_y_hat_1_given_y_0))
+    + sum(n_classified_negative_annotated_negative_by_area .* log((1 - p_y) * (1 - p_y_hat_1_given_y_0))));
+}
