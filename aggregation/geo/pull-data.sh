baabafcfee689623f92230cd9470ed11baecbe34
Matt Franchi <mattwfranc@gmail.com>
2024-10-22 14:37:41 -0400

    geographic datasets added to pull data

--- aggregation/geo/pull-data.sh
+++ aggregation/geo/pull-data.sh
@@ -21,3 +21,10 @@ wget -O data/cb-nyc-wi-2020.geojson 'https://services5.arcgis.com/GfwWNkhOj9bNBq
 
 ## 2020 NYC Census Blocks, water areas clipped
 wget -O data/cb-nyc-2020.geojson 'https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/NYC_Census_Blocks_for_2020_US_Census/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=pgeojson'
+
+## NYC Integer 1 foot Digital Elevation Model Raster 
+wget -O data/nyc-1ft-dem.zip 'https://sa-static-customer-assets-us-east-1-fedramp-prod.s3.amazonaws.com/data.cityofnewyork.us/NYC_DEM_1ft_Int.zip'
+unzip -d data data/nyc-1ft-dem.zip
+
+# delete zip
+rm data/nyc-1ft-dem.zip
