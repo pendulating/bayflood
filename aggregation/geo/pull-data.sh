513f37156e8ceaac50ce579c0cbf68f244a81978
Matt Franchi <mattwfranc@gmail.com>
2024-10-23 14:00:08 -0400

    fixed typo between water clipped and not clipped nyc cts

--- aggregation/geo/pull-data.sh
+++ aggregation/geo/pull-data.sh
@@ -10,11 +10,11 @@ mkdir -p data
 
 # Geographic Boundaries 
 
-## 2020 NYC Census Tracts, including water areas 
-wget -O data/ct-nyc-wi-2020.geojson 'https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/NYC_Census_Tracts_for_2020_US_Census/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=pgeojson'
+## 2020 NYC Census Tracts, water areas clipped
+wget -O data/ct-nyc-2020.geojson 'https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/NYC_Census_Tracts_for_2020_US_Census/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=pgeojson'
 
-## 2020 NYC Census Tracts, water areas clipped 
-wget -O data/ct-nyc-2020.geojson 'https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/NYC_Census_Tracts_for_2020_US_Census_Water_Included/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=pgeojson'
+## 2020 NYC Census Tracts, including water areas
+wget -O data/ct-nyc-wi-2020.geojson 'https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/NYC_Census_Tracts_for_2020_US_Census_Water_Included/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=pgeojson'
 
 ## 2020 NYC Census Blocks, including water areas 
 wget -O data/cb-nyc-wi-2020.geojson 'https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/NYC_Census_Blocks_for_2020_US_Census_Water_Included/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=pgeojson'
