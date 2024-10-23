6c5af81967ec58ee7621ca7ababb4d0355845f93
Matt Franchi <mattwfranc@gmail.com>
2024-10-23 15:13:36 -0400

    downsampling and zonal stats for ct nyc

--- aggregation/geo/pp_topology.py
+++ aggregation/geo/pp_topology.py
@@ -1,5 +1,16 @@
 import os 
 import rasterio
+from rasterio.enums import Resampling
+from rasterstats import zonal_stats
+from itertools import islice
+import pandas as pd
+import geopandas as gpd 
+
+import sys 
+sys.path.append('../../')
+from logger import setup_logger
+logger = setup_logger('pp-topology-subroutine')
+logger.setLevel("INFO")
 
 
 def downsample_raster(TOPOGRAPHY_NYC, downsample_factor=10, REGEN_TOPOLOGY=False, OUTPUT_PATH=''):
@@ -29,10 +40,48 @@ def downsample_raster(TOPOGRAPHY_NYC, downsample_factor=10, REGEN_TOPOLOGY=False
             "transform": new_transform
         })
 
+        logger.success("Downsampling complete")
+
         # Write the new raster to disk
         if REGEN_TOPOLOGY: 
+            logger.info(f"Writing downsampled topology to {OUTPUT_PATH}")
             os.makedirs(os.path.dirname(OUTPUT_PATH), exist_ok=True)
             with rasterio.open(OUTPUT_PATH, "w", **new_meta) as dst:
                 dst.write(topology)
 
+
+
+def sample_topology(topology_path, sampling_geom,save=True):
+
+    # make sure topology_path ends in .tif
+    assert topology_path.endswith('.tif'), "topology_path must end in .tif"
+
+    sampling_geom = gpd.read_file(sampling_geom).to_crs("EPSG:2263")
+
+    summary_stats = zonal_stats( 
+        sampling_geom,
+        topology_path,
+    )
+
     
+
+    # summary_stats is a list of dicts 
+    # each dict has the same keys
+    # convert summary_stats to df 
+    summary_stats_df = pd.DataFrame(summary_stats)
+
+    logger.success("Topology sampling complete")
+
+    if save: 
+        logger.info("Saving topology sampling to data/processed/ct_nyc_topology.csv")
+        summary_stats_df.to_csv('data/processed/ct_nyc_topology.csv')
+
+
+
+
+if __name__ == '__main__': 
+
+    downsample_raster("data/DEM_LiDAR_1ft_2010_Improved_NYC_int.tif", downsample_factor=10, REGEN_TOPOLOGY=True, OUTPUT_PATH='data/processed/topology_nyc_downsampled.tif')
+    sample_topology('data/processed/topology_nyc_downsampled.tif', 'data/ct-nyc-2020.geojson', save=True)
+
+    logger.success("Topology sampling complete")
