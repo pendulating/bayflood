ddb8983adf047e2aeb20cf3eef0fcd3700457ec7
Matt Franchi <mattwfranc@gmail.com>
2025-02-05 16:23:11 -0500

    updated constants file

--- notebooks/for_paper/constants.py
+++ notebooks/for_paper/constants.py
@@ -1,4 +1,9 @@
+CURRENT_DF="/share/ju/matt/street-flooding/runs/icar_icar/simulated_False/ahl_True/covariates_True/FEB5_FINAL_PRE_KDD_MODEL_20250205-1603/analysis_df_FEB5_FINAL_PRE_KDD_MODEL_02052025.csv"
 PAPER_PATH='/share/ju/matt/street-flooding/KDD-2025-Matt-Flooding-Paper'
 WGS='EPSG:4326'
 PROJ='EPSG:2263'
 GEO_PATH='../../aggregation/geo/data'
+
+
+CURRENT_ADJ_1="/share/ju/matt/street-flooding/data/adjacency/cg_500/ct_nyc_adj_list_custom_geometric_node1.txt"
+CURRENT_ADJ_2="/share/ju/matt/street-flooding/data/adjacency/cg_500/ct_nyc_adj_list_custom_geometric_node2.txt"
