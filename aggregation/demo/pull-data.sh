b452cd41e44035e6ed0c1812ed3baca0f18f7cc0
Matt Franchi <mattwfranc@gmail.com>
2025-02-10 11:06:07 -0500

    feb 10 demo data sync

--- aggregation/demo/pull-data.sh
+++ aggregation/demo/pull-data.sh
@@ -3,47 +3,44 @@ mkdir -p data
 # key.text should only contain your key, and be in the same directory as this script. 
 API_KEY=$(cat key.txt)
 
-# ACS DP05 - NYC 
-acs22_dp05_md='https://api.census.gov/data/2022/acs/acs5/profile/groups/DP05.json'
-wget -O data/acs22_dp05_md.json "${acs22_dp05_md}?key=${API_KEY}"
+for year in $(seq 2023 2023)
+do
 
-acs22_dp05_template='https://api.census.gov/data/2022/acs/acs5/profile?get=group(DP05)&for=tract:*&in=state:36&in=county:005,047,061,081,085'
+    # ACS DP05 - NYC 
+    acs_dp05_md='https://api.census.gov/data/'${year}'/acs/acs5/profile/groups/DP05.json'
+    wget -O data/acs${year}_dp05_md.json "${acs_dp05_md}"
 
-# wget the group 
-wget -O data/acs22_dp05.json "${acs22_dp05_template}&key=${API_KEY}"
+    acs_dp05_template='https://api.census.gov/data/'${year}'/acs/acs5/profile?get=group(DP05)&for=tract:*&in=state:36&in=county:005,047,061,081,085'
+    wget -O data/acs${year}_dp05.json "${acs_dp05_template}&key=${API_KEY}"
 
+    # Internet Access 
+    acs_s2801_md='https://api.census.gov/data/'${year}'/acs/acs5/subject/groups/S2801.json'
+    wget -O data/acs${year}_s2801_md.json "${acs_s2801_md}"
 
-# Internet Access 
-acs22_s2801_md='https://api.census.gov/data/2022/acs/acs5/subject/groups/S2801.json'
-wget -O data/acs22_s2801_md.json "${acs22_s2801_md}?key=${API_KEY}"
+    acs_s2801_template='https://api.census.gov/data/'${year}'/acs/acs5/subject?get=group(S2801)&for=tract:*&in=state:36&in=county:005,047,061,081,085'
+    wget -O data/acs${year}_s2801.json "${acs_s2801_template}&key=${API_KEY}"
 
-acs22_s2801_template='https://api.census.gov/data/2022/acs/acs5/subject?get=group(S2801)&for=tract:*&in=state:36&in=county:005,047,061,081,085'
-# wget the group
-wget -O data/acs22_s2801.json "${acs22_s2801_template}&key=${API_KEY}"
+    # Median Household Income
+    acs_s1901_md='https://api.census.gov/data/'${year}'/acs/acs5/subject/groups/S1901.json'
+    wget -O data/acs${year}_s1901_md.json "${acs_s1901_md}"
 
-# Median Household Income
-acs22_s1901_md='https://api.census.gov/data/2022/acs/acs5/subject/groups/S1901.json'
-wget -O data/acs22_s1901_md.json "${acs22_s1901_md}?key=${API_KEY}"
+    acs_s1901_template='https://api.census.gov/data/'${year}'/acs/acs5/subject?get=group(S1901)&for=tract:*&in=state:36&in=county:005,047,061,081,085'
+    wget -O data/acs${year}_s1901.json "${acs_s1901_template}&key=${API_KEY}"
 
-acs22_s1901_template='https://api.census.gov/data/2022/acs/acs5/subject?get=group(S1901)&for=tract:*&in=state:36&in=county:005,047,061,081,085'
-# wget the group
-wget -O data/acs22_s1901.json "${acs22_s1901_template}&key=${API_KEY}"
+    # Educational Attainment 
+    acs_s1501_md='https://api.census.gov/data/'${year}'/acs/acs5/subject/groups/S1501.json'
+    wget -O data/acs${year}_s1501_md.json "${acs_s1501_md}"
 
-# Educational Attainment 
-acs22_s1501_md='https://api.census.gov/data/2022/acs/acs5/subject/groups/S1501.json'
-wget -O data/acs22_s1501_md.json "${acs22_s1501_md}?key=${API_KEY}"
+    acs_s1501_template='https://api.census.gov/data/'${year}'/acs/acs5/subject?get=group(S1501)&for=tract:*&in=state:36&in=county:005,047,061,081,085'
+    wget -O data/acs${year}_s1501.json "${acs_s1501_template}&key=${API_KEY}"
 
-acs22_s1501_template='https://api.census.gov/data/2022/acs/acs5/subject?get=group(S1501)&for=tract:*&in=state:36&in=county:005,047,061,081,085'
-# wget the group
-wget -O data/acs22_s1501.json "${acs22_s1501_template}&key"
+    # Limited English Speaking Households 
+    acs_s1602_md='https://api.census.gov/data/'${year}'/acs/acs5/subject/groups/S1602.json'
+    wget -O data/acs${year}_s1602_md.json "${acs_s1602_md}"
 
-# Limited English Speaking Households 
-acs22_s1602_md='https://api.census.gov/data/2022/acs/acs5/subject/groups/S1602.json'
+    acs_s1602_template='https://api.census.gov/data/'${year}'/acs/acs5/subject?get=group(S1602)&for=tract:*&in=state:36&in=county:005,047,061,081,085'
+    wget -O data/acs${year}_s1602.json "${acs_s1602_template}&key=${API_KEY}"
 
-wget -O data/acs22_s1602_md.json "${acs22_s1602_md}?key=${API_KEY}"
+   
 
-acs22_s1602_template='https://api.census.gov/data/2022/acs/acs5/subject?get=group(S1602)&for=tract:*&in=state:36&in=county:005,047,061,081,085'
-# wget the group
-
-wget -O data/acs22_s1602.json "${acs22_s1602_template}&key"
-
+done
