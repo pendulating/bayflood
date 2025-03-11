b15c2861b7c16349cbec332cac85bd0ba3be4f14
Matt Franchi <mattwfranc@gmail.com>
2025-03-11 12:43:15 -0400

    updated constants to include ESTIMATE_TO_USE parameter

--- notebooks/for_paper/constants.py
+++ notebooks/for_paper/constants.py
@@ -1,9 +1,17 @@
-CURRENT_DF="/share/ju/matt/street-flooding/runs/icar_icar/simulated_False/ahl_True/covariates_True/FEB5_FINAL_PRE_KDD_MODEL_20250205-1603/analysis_df_FEB5_FINAL_PRE_KDD_MODEL_02052025.csv"
+CURRENT_DF="/share/ju/matt/street-flooding/runs/icar_icar/simulated_False/ahl_True/covariates_True/FEB7_FINAL_KDD_MODEL_20250207-1732/analysis_df_FEB7_FINAL_KDD_MODEL_02072025.csv"
+
+CURRENT_NO_COVARIATES_DF="/share/ju/matt/street-flooding/runs/icar_icar/simulated_False/ahl_True/covariates_False/FEB7_FINAL_KDD_MODEL_NO_COVARIATES_20250207-1814/analysis_df_FEB7_FINAL_KDD_MODEL_NO_COVARIATES_02072025.csv"
+
 PAPER_PATH='/share/ju/matt/street-flooding/KDD-2025-Flooding-Paper'
+
+DELIVERABLES_PATH='/share/ju/matt/street-flooding/deliverables'
 WGS='EPSG:4326'
 PROJ='EPSG:2263'
 GEO_PATH='../../aggregation/geo/data'
 
+CURRENT_PP_BASELINES_GLOB="/share/ju/matt/street-flooding/runs/icar_icar/simulated_False/ahl_True/covariates_True/MAR4_25K*/performance_on_baselines.csv"
 
 CURRENT_ADJ_1="/share/ju/matt/street-flooding/data/adjacency/cg_500/ct_nyc_adj_list_custom_geometric_node1.txt"
 CURRENT_ADJ_2="/share/ju/matt/street-flooding/data/adjacency/cg_500/ct_nyc_adj_list_custom_geometric_node2.txt"
+
+ESTIMATE_TO_USE='confirmed_or_above_thres'
