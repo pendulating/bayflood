c04c771af56d2c13af6026318631329956054110
Matt Franchi <mattwfranc@gmail.com>
2025-09-10 16:28:19 -0400

    refresh cache docstring clarity

--- refresh_cache.py
+++ refresh_cache.py
@@ -2,19 +2,26 @@ from logger import setup_logger
 
 import os
 import shutil
+from typing import Optional
 
 logger = setup_logger("cache-refresh-subroutine")
 logger.setLevel("INFO")
 
-def refresh_cache(netid):
-    if 'emmapierson/Desktop' in os.getcwd():
-        CACHE_PATH = '/Users/emmapierson/Desktop/git_repos/street-flooding/.cache/httpstan'
+
+def refresh_cache(base_dir: Optional[str] = None):
+    """Clear and recreate PyStan/httpstan cache directory.
+
+    If base_dir is None, uses the current user's home directory.
+    """
+    if base_dir:
+        cache_path = os.path.join(base_dir, ".cache", "httpstan")
     else:
-        CACHE_PATH = f'/home/{netid}/.cache/httpstan'
-    logger.info(f"Refreshing cache at {CACHE_PATH}")
-    if os.path.exists(CACHE_PATH):
-        logger.info(f"Removing {CACHE_PATH}")
-        shutil.rmtree(CACHE_PATH)
-    logger.info(f"Creating {CACHE_PATH}")
-    os.makedirs(CACHE_PATH)
+        cache_path = os.path.expanduser("~/.cache/httpstan")
+
+    logger.info(f"Refreshing cache at {cache_path}")
+    if os.path.exists(cache_path):
+        logger.info(f"Removing {cache_path}")
+        shutil.rmtree(cache_path)
+    logger.info(f"Creating {cache_path}")
+    os.makedirs(cache_path, exist_ok=True)
     logger.success("Successfully refreshed cache.")
